929617dad1b4fa08f2f564c6c15e7b87cda53ebe
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-07-02 13:19:08 +0530

    feat: add styles to login page

--- src/Pages/Students/LoginSignUp.jsx
+++ src/Pages/Students/LoginSignUp.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./CSS/LoginSignup.css";
 import logo from "../../Components/Assets/logo.png";
+import student from "../../Components/Assets/student.png";
+import tutor from "../../Components/Assets/tutor.png";
+import youtube from "../../Components/Assets/youtube.png";
+import facebook from "../../Components/Assets/facebook.png";
+import instagram from "../../Components/Assets/instagram.png";
+import whatsapp from "../../Components/Assets/whatsapp.png";
 function LoginSignUp() {
   return (
     <div className="mainContainer">
@@ -13,8 +19,34 @@ function LoginSignUp() {
         <div className="innerContainer">
           <p>Select Your Role</p>
           <div className="buttonContainer">
-            <div className="role">Student</div>
-            <div className="role">Tutor</div>
+            <div className="role">
+              <img src={student} alt="student" />
+              Student
+            </div>
+            <div className="role">
+              <img src={tutor} alt="tutor" />
+              Tutor
+            </div>
+          </div>
+          <div className="continue">
+            <button>Continue</button>
+          </div>
+          <div className="dotts">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="socials">
+            <img src={facebook} alt="" />
+
+            <img src={instagram} alt="" />
+
+            <img src={youtube} alt="" />
+
+            <img src={whatsapp} alt="" />
+          </div>
+          <div className="footer">
+            <span>Powered by DevSparks Solutions</span>
           </div>
         </div>
       </div>
