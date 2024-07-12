3d0d3df8b2961b83cf1ff7f614e171f3b65075be
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-07-12 19:26:27 +0530

    feat: add event calendar

--- src/Pages/Students/Dashboard.jsx
+++ src/Pages/Students/Dashboard.jsx
@@ -3,14 +3,22 @@ import "./CSS/Dashboard.css";
 import Navbar from "../../Components/Navbar/Navbar";
 import Header from "../../Components/Header/Header";
 import Banner from "../../Components/Banner/Banner";
+import EventCalendar from "../../Components/EventCalendar/EventCalendar";
 function Dashboard() {
   return (
     <div className="dash">
       <Navbar />
+
       <div className="right">
         <Header />
-        <div className="right-left">
-          <Banner />
+        <div className="rights">
+          <div className="right-left">
+            <Banner />
+          </div>
+          <div className="right-right">
+            <p>Calendar</p>
+            <EventCalendar />
+          </div>
         </div>
       </div>
     </div>
