3ac7413fc4832567ac7243737b37e32107f03d2c
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-07-12 17:49:50 +0530

    feat: add header and banner for dashboards

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Banner.css";
+function Banner() {
+  return (
+    <div className="banner">
+      <p>July 12 , 2024</p>
+      <span>Welcome back, Didula!</span>
+      <h5>Always stay updated in your student portal</h5>
+    </div>
+  );
+}
+
+export default Banner;
--- src/Pages/Students/Dashboard.jsx
+++ src/Pages/Students/Dashboard.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import "./CSS/Dashboard.css";
 import Navbar from "../../Components/Navbar/Navbar";
+import Header from "../../Components/Header/Header";
+import Banner from "../../Components/Banner/Banner";
 function Dashboard() {
   return (
-    <div>
+    <div className="dash">
       <Navbar />
+      <div className="right">
+        <Header />
+        <div className="right-left">
+          <Banner />
+        </div>
+      </div>
     </div>
   );
 }
