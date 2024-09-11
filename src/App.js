a3f436b6423adf3487051c74c7d272582d825054
Sanduni Lakshika <110313255+slakshika2026@users.noreply.github.com>
2024-09-11 13:44:09 +0530

    feat:

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import LoginSignUp from "./Pages/Students/LoginSignUp";
 import Dashboard from "./Pages/Students/Dashboard";
 import MyProfile from "./Pages/Students/MyProfile";
+import Courses from "./Pages/Students/Courses";
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element={<LoginSignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/profile" element={<MyProfile />} />
-
+        <Route path="/courses" element={<Courses />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Pages/Students/Courses.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import Header from "../../Components/Header/Header";
+
+function Courses() {
+
+   return (
+      <>
+         <div className="dash">
+            <Navbar />
+            <Header />
+
+
+
+         </div>
+
+
+
+
+
+      </>
+
+   )
+}
+export default Courses;
