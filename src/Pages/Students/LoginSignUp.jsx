0aa71625820e905bc0e35570720ed2687ef8c39d
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-09-19 09:28:48 +0530

    feat: bug fix

--- src/Pages/Students/LoginSignUp.jsx
+++ src/Pages/Students/LoginSignUp.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./CSS/LoginSignup.css";
-
 import logo from "../../Components/Assets/logo.png";
 import student from "../../Components/Assets/student.png";
 import tutor from "../../Components/Assets/tutor.png";
@@ -10,7 +9,6 @@ import instagram from "../../Components/Assets/instagram.png";
 import whatsapp from "../../Components/Assets/whatsapp.png";
 import person from "../../Components/Assets/person.png";
 import lock from "../../Components/Assets/lock.png";
-import RegContent from "../../Components/RegContent/RegContent";
 
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../firebase";
@@ -28,8 +26,6 @@ function LoginSignUp() {
   const [tutorBgColor, setTutorBgColor] = useState("#f9f9f7");
   const [currentDot, setCurrentDot] = useState(0);
   const [showLogin, setShowLogin] = useState(false);
-  const [showRegister, setShowRegister] = useState(false);
-  const [showRightContainer, setShowRightContainer] = useState(true);
   const [selectedRole, setSelectedRole] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -62,8 +58,6 @@ function LoginSignUp() {
     }
     setCurrentDot(1);
     setShowLogin(true);
-
-    setShowRegister(false);
   };
 
   const handleLogin = async (e) => {
@@ -76,7 +70,6 @@ function LoginSignUp() {
       setError(err.message);
     }
   };
-
 
   const handleForgotPasswordClick = () => {
     setShowForgotPassword(true);
@@ -132,56 +125,9 @@ function LoginSignUp() {
     }
   };
 
-  const handleRegister = () => {
-    setShowRegister(true);
-    setShowLogin(false);
-    setShowRightContainer(false);
-    setCurrentDot(2);
-  }
-
-
   return (
     <div className="mainContainer">
-      {showRegister ? (
-        <>
-          <RegContent />
-        </>
-      ) : (
-        <></>
-      )}
-
       <div className="leftContainer"></div>
-      {showRightContainer ? (<>
-        <div className="rightContainer">
-          <div className="logoContainer">
-            <img src={logo} alt="logo" />
-          </div>
-          <div className="innerContainer">
-            {showLogin ? (
-              <>
-                <span className="signin">Sign In</span>
-                <div className="inputContainer">
-                  <img src={person} alt=""></img>
-                  <input
-                    type="text"
-                    placeholder="Email Address"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                  />
-                </div>
-                <div className="inputContainer">
-                  <img src={lock} alt=""></img>
-                  <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                  />
-                  {/* <img className="eye" src={eye} alt=""></img> */}
-                </div>
-
 
       <div className="rightContainer">
         <div className="logoContainer">
@@ -296,101 +242,74 @@ function LoginSignUp() {
                 >
                   <img src={student} alt="student" />
                   Student
-
-                <span className="forgetPassword">Forgot Password?</span>
-                <div className="loginButton">
-                  <button onClick={handleLogin}>Login</button>
                 </div>
-
-                <div className="registration">
-                  <span>
-                    <button
-                      onClick={handleRegister}>NOT A STUDENT? REGISTER NOW!</button></span>
-
-
+                <div
+                  className="role"
+                  onClick={handleTutorClick}
+                  style={{ backgroundColor: tutorBgColor }}
+                >
+                  <img src={tutor} alt="tutor" />
+                  Tutor
                 </div>
-              </>
-            ) : (
-              <>
-                <p>Select Your Role</p>
-                <div className="buttonContainer">
-                  <div
-                    className="role"
-                    onClick={handleStudentClick}
-                    style={{ backgroundColor: studentBgColor }}
-                  >
-                    <img src={student} alt="student" />
-                    Student
-                  </div>
-                  <div
-                    className="role"
-                    onClick={handleTutorClick}
-                    style={{ backgroundColor: tutorBgColor }}
-                  >
-                    <img src={tutor} alt="tutor" />
-                    Tutor
-                  </div>
-                </div>
-                <div className="continue">
-                  <button onClick={handleContinueClick}>Continue</button>
-                </div>
-              </>
-            )}
+              </div>
+              <div className="continue">
+                <button onClick={handleContinueClick}>Continue</button>
+              </div>
+            </>
+          )}
 
-            <div className="dotts">
-              <span
-                style={{
-                  backgroundColor: currentDot === 0 ? "#F6CB52" : "#D9D9D9",
-                }}
-              ></span>
-              <span
-                style={{
-                  backgroundColor: currentDot === 1 ? "#F6CB52" : "#D9D9D9",
-                }}
-              ></span>
-              <span
-                style={{
-                  backgroundColor: currentDot === 2 ? "#F6CB52" : "#D9D9D9",
-                }}
-              ></span>
-            </div>
-            <div className="socials">
-              <a
-                href="https://www.facebook.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={facebook} alt="Facebook" />
-              </a>
-              <a
-                href="https://www.instagram.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={instagram} alt="Instagram" />
-              </a>
-              <a
-                href="https://www.youtube.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={youtube} alt="YouTube" />
-              </a>
-              <a
-                href="https://www.whatsapp.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={whatsapp} alt="WhatsApp" />
-              </a>
-            </div>
-            <div className="footer">
-              <span>Powered by DevSparks Solutions</span>
-            </div>
+          <div className="dotts">
+            <span
+              style={{
+                backgroundColor: currentDot === 0 ? "#F6CB52" : "#D9D9D9",
+              }}
+            ></span>
+            <span
+              style={{
+                backgroundColor: currentDot === 1 ? "#F6CB52" : "#D9D9D9",
+              }}
+            ></span>
+            <span
+              style={{
+                backgroundColor: currentDot === 2 ? "#F6CB52" : "#D9D9D9",
+              }}
+            ></span>
+          </div>
+          <div className="socials">
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={facebook} alt="Facebook" />
+            </a>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={instagram} alt="Instagram" />
+            </a>
+            <a
+              href="https://www.youtube.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={youtube} alt="YouTube" />
+            </a>
+            <a
+              href="https://www.whatsapp.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={whatsapp} alt="WhatsApp" />
+            </a>
+          </div>
+          <div className="footer">
+            <span>Powered by DevSparks Solutions</span>
           </div>
         </div>
-      </>) : (<></>)}
-
+      </div>
     </div>
   );
 }
